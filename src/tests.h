cfc1b2f4b37e098a076d267139e7f7007c90e6f0
marcoscastro <mcastrosouza@live.com>
2015-11-20 19:19:07 -0200

    added more 1 test for levenshtein distance

--- src/tests.h
+++ src/tests.h
@@ -9,6 +9,7 @@ public:
 	void run()
 	{
 		testLevenshteinDistance1();
+		testLevenshteinDistance2();
 	}
 
 	bool testLevenshteinDistance1()
@@ -26,6 +27,24 @@ public:
 		}
 
 		std::cout << "Fail in the test: testLevenshteinDistance1\n";
+		return false;
+	}
+
+	bool testLevenshteinDistance2()
+	{
+		std::string s1("CCCCTGACACACTG"), s2("CTCTG");
+		int expected = 9;
+
+		Distance dist(s1, s2);
+		int result = dist.levenshteinDistance();
+
+		if(result == expected)
+		{
+			std::cout << "Success in the test: testLevenshteinDistance2\n";
+			return true;
+		}
+
+		std::cout << "Fail in the test: testLevenshteinDistance2\n";
 		return false;
 	}
 };
